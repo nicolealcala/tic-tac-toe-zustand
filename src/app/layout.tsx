0fc5860d70a0b9495916271995d683767bc7d710
Nicole Alcala <nicolealcalq@gmail.com>
2026-08-17 18:25:56 +0800

    Updated brand font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { BBH_Hegarty } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const font = BBH_Hegarty({
+  variable: "--font-brand",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -19,11 +15,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${font.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col justify-center items-center">
+        {children}
+      </body>
     </html>
   );
 }
